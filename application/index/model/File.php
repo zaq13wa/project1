690c62675eaef1e0ed65ef5290afbc2dcf001b73
zaq13wa <32063128+zaq13wa@users.noreply.github.com>
2018-04-23 22:46:06 +0800

    ver0.68

--- application/index/model/File.php
+++ application/index/model/File.php
@@ -3,6 +3,34 @@ namespace app\index\model;
 use think\Model;
 class File extends Model
 {
+	protected static function init()
+    {
+      	File::event('before_insert',function($article){
+          if($_FILES['img']['tmp_name']){
+                $file = request()->file('img');
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if($info){
+                    // $thumb=ROOT_PATH . 'public' . DS . 'uploads'.'/'.$info->getExtension();
+                    $thumb= 'public' . DS .'uploads'.'/'.$info->getSaveName();
+                    $article['thumb']=$thumb;
+                }
+            }
+      });
+
+
+      	File::event('before_update',function($article){
+      
+      });
+
+      	File::event('before_delete',function($article){
+          
+          		$arts=File::find($article->id);
+          		$thumbpath=$_SERVER['DOCUMENT_ROOT'].$arts['thumb'];
+                if(file_exists($thumbpath)){
+                	@unlink($thumbpath);
+                }
+        });
+    }
 	 public function user()
     {
         return $this->belongsTo('User','uid','user_id');

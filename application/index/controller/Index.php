1f815c8965e44f9e666f4cd7bffc59a239ae3beb
zaq13wa <32063128+zaq13wa@users.noreply.github.com>
2018-06-05 23:47:15 +0800

    ver1.1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -61,5 +61,52 @@ class Index extends Common
      	$this->assign('user',$u->ToArray());
         return view();
      }
-
+     public function msg(){
+        $u=Model('User')->get(session('user_id'));
+      unset($u['password']);
+          if(!$u['groups']=$u->groupt)
+          {
+             $u['groups']=$u->usergroup;
+             unset($u["usergroup"]);
+             foreach( $u['groups'] as $g){
+               $g['Id']=$g['group']['Id'];
+               $g["name"]=$g['group']['name'];
+               $g["leader"]=$g['group']['leader'];
+               $g['pid']=$g->group->course["name"];
+               $g['teacher']=$g->group->teacher['name'];
+               unset($g['group']);
+             }
+          }
+          else{
+             foreach( $u['groups'] as $g){
+              $g['pid']=$g->course['name'];
+              $g['teacher']=session('username');
+              unset($g['course']);
+             } 
+          }
+         unset($u['groupt']);
+      //dump($u->ToArray());die;
+         $u=$u->ToArray();
+         $this->assign('g',$u['groups']);
+        return view();
+     }
+     public function msgsend(){
+         $data=input('get.');
+         $data['uid']=session('user_id');
+         $data['time']=date('Y-m-d h:i:s');
+         dump($data);
+         db('msg')->insert($data);
+         return $data;
+     }
+     public function msgshow(){
+         $data=input('get.gid');
+         $s=db('msg')->where('gid',$data)->order('time')->select();
+         foreach($s as &$a)
+         {
+            $u= db('user')->where('user_id',$a['uid'])->find();
+            $a['uid']=$u['name'];
+            $a['time']=date("H:i:s",strtotime($a['time']));
+         }
+         return $s;
+     }
 }
